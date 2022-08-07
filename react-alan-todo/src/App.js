ffc42d918cca5a1eb1fbdfeeac4e8bee82b5035b
Aditya Nigam <nigamadi2209@gmail.com>
2022-08-07 11:18:30 +0530

    initial changes

---
+++ react-alan-todo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+// import Todo from './components/Todo';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <NavBar/>
+    
+
+  );
+}
+
+export default App;
